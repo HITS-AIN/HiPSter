fix jpg size, color and scaling

src/hipster/hips_generator.py
@@ -48,7 +48,7 @@ class HiPSGenerator:
                 )
             recon = self.reconstruction(vectors)
             image = self.generate_tile(recon, i, j, self.hierarchy, 0)
-            image = Image.fromarray((np.clip(image, 0, 1) * 255).astype(np.uint8))
+            image = Image.fromarray(image)
             image.save(
                 os.path.join(
                     self.output_folder,

src/hipster/spectrum_plotter.py
@@ -42,6 +42,10 @@ class SpectrumPlotter:
     def __call__(self, spectrum: np.ndarray) -> plt.Axes:
 
         fig, ax = plt.subplots(figsize=(self.figsize, self.figsize), dpi=self.dpi)
+        # fig = plt.figure(figsize=(self.figsize, self.figsize), dpi=self.dpi)
+        # ax = fig.gca()
+        # ax.axis("tight")
+
         ax.plot(self.wavelengths, spectrum, color="black")
 
         y = np.linspace(0, 6, 100)
@@ -73,7 +77,7 @@ class SpectrumPlotter:
             return ax
         elif self.return_type == "ndarray":
             canvas = fig.canvas
-            canvas.draw()
+            canvas.draw_idle()
             data = np.frombuffer(canvas.tostring_rgb(), dtype="uint8")
             data = data.reshape(*reversed(canvas.get_width_height()), 3)
             return data

tests/test_hips_generator.py
@@ -1,13 +1,17 @@
 import numpy as np
+import pytest
 
 from hipster import HiPSGenerator, Reconstruction, SpectrumPlotter
 
 
-def test_hips_generator():
+def test_hips_generator(tmp_path):
 
     hips_generator = HiPSGenerator(
         reconstruction=Reconstruction("tests/models/vae_decoder.onnx"),
-        image_maker=SpectrumPlotter(np.arange(336, 1023, 2)),
+        image_maker=SpectrumPlotter(
+            np.arange(336, 1023, 2), ylim=(0, 1), figsize_in_pixel=400
+        ),
+        output_folder=tmp_path,
     )
 
     hips_generator(max_order=1)