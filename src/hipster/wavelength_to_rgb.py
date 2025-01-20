d29c0bcede6d7982d471bd0c862515009a5bcebf
Bernd Doser <bernd.doser@h-its.org>
2025-01-20 10:54:08 +0100

    fix wavelength to rgb

--- src/hipster/wavelength_to_rgb.py
+++ src/hipster/wavelength_to_rgb.py
@@ -1,54 +1,47 @@
 def wavelength_to_rgb(wavelength, gamma=0.8):
-    """
-    Approximate conversion from wavelength (nm) to RGB color.
-    Adapted from Dan Bruton's algorithm: http://www.midnightkite.com/color.html
+    """This converts a given wavelength of light to an
+    approximate RGB color value. The wavelength must be given
+    in nanometers in the range from 380 nm through 750 nm
+    (789 THz through 400 THz).
+    Based on code by Dan Bruton
+    http://www.physics.sfasu.edu/astro/color/spectra.html
     """
 
     # Clamp wavelength to the visible range
     if wavelength < 380:
         wavelength = 380
-    if wavelength > 780:
-        wavelength = 780
-    R = G = B = 0.0
-    if 380 <= wavelength < 440:
-        t = (wavelength - 380) / (440 - 380)
-        R = -t
+    if wavelength > 750:
+        wavelength = 750
+
+    wavelength = float(wavelength)
+    if wavelength >= 380 and wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
         G = 0.0
-        B = 1.0
-    elif 440 <= wavelength < 490:
+        B = (1.0 * attenuation) ** gamma
+    elif wavelength >= 440 and wavelength <= 490:
         R = 0.0
-        t = (wavelength - 440) / (490 - 440)
-        G = t
+        G = ((wavelength - 440) / (490 - 440)) ** gamma
         B = 1.0
-    elif 490 <= wavelength < 510:
+    elif wavelength >= 490 and wavelength <= 510:
         R = 0.0
         G = 1.0
-        t = (wavelength - 490) / (510 - 490)
-        B = 1.0 - t
-    elif 510 <= wavelength < 580:
-        R = (wavelength - 510) / (580 - 510)
+        B = (-(wavelength - 510) / (510 - 490)) ** gamma
+    elif wavelength >= 510 and wavelength <= 580:
+        R = ((wavelength - 510) / (580 - 510)) ** gamma
         G = 1.0
         B = 0.0
-    elif 580 <= wavelength < 645:
+    elif wavelength >= 580 and wavelength <= 645:
         R = 1.0
-        t = (wavelength - 580) / (645 - 580)
-        G = 1.0 - t
+        G = (-(wavelength - 645) / (645 - 580)) ** gamma
         B = 0.0
-    elif 645 <= wavelength <= 780:
-        R = 1.0
+    elif wavelength >= 645 and wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        R = (1.0 * attenuation) ** gamma
         G = 0.0
         B = 0.0
-    # Intensity correction near range edges
-    if 380 <= wavelength < 420:
-        alpha = 0.3 + 0.7 * (wavelength - 380) / 40
-    elif 420 <= wavelength <= 700:
-        alpha = 1.0
-    elif 700 < wavelength <= 780:
-        alpha = 0.3 + 0.7 * (780 - wavelength) / 80
     else:
-        alpha = 0.0
-    # Gamma correction
-    R = (R * alpha) ** gamma
-    G = (G * alpha) ** gamma
-    B = (B * alpha) ** gamma
+        R = 0.0
+        G = 0.0
+        B = 0.0
     return (R, G, B)
