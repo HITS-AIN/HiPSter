d1821be671236c6fcde92ae8a1961f825a4c4725
Bernd Doser <bernd.doser@h-its.org>
2025-04-21 16:06:32 +0200

    add data_column to VOTableGenerator

--- src/hipster/votable_generator.py
+++ src/hipster/votable_generator.py
@@ -21,6 +21,7 @@ class VOTableGenerator(Task):
         self,
         encoder: Inference,
         data_directory: str,
+        data_column: str,
         output_file: str = "votable.vot",
         url: str = "http://localhost:8083",
         batch_size: int = 256,
@@ -35,6 +36,7 @@ class VOTableGenerator(Task):
         Args:
             encoder (callable): Function that encodes the data.
             data_directory (str): The directory containing the data.
+            data_column (str): The column name of the data.
             output_file (str, optional): The output file name. Defaults to "votable.xml".
             url (str): The URL of the HiPS server. Defaults to "http://localhost:8083".
             batch_size (int, optional): The batch size to use. Defaults to 256.
@@ -47,6 +49,7 @@ class VOTableGenerator(Task):
         super().__init__("VOTableGenerator", **kwargs)
         self.encoder = encoder
         self.data_directory = data_directory
+        self.data_column = data_column
         self.output_file = output_file
         self.url = url
         self.batch_size = batch_size
@@ -69,14 +72,14 @@ class VOTableGenerator(Task):
         # dataset = dataset.filter(ds.field("source_id") % 10 == 0)
 
         # Reshape the data if the shape is stored in the metadata.
-        metadata_shape = b"flux_shape"
+        metadata_shape = bytes(self.data_column, "utf8") + b"_shape"
         if dataset.schema.metadata and metadata_shape in dataset.schema.metadata:
             shape_string = dataset.schema.metadata[metadata_shape].decode("utf8")
             shape = shape_string.replace("(", "").replace(")", "").split(",")
             shape = tuple(map(int, shape))
 
         for batch in dataset.to_batches(batch_size=self.batch_size):
-            flux = batch["flux"].flatten().to_numpy().reshape(-1, *shape)
+            flux = batch[self.data_column].flatten().to_numpy().reshape(-1, *shape)
 
             # if flux.shape[0] != self.batch_size:
             #     print(f"Skipping batch with shape {flux.shape}")
--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -6,6 +6,7 @@ def test_catalog_generator():
     votable_generator = VOTableGenerator(
         encoder=Inference("tests/models/vae_encoder.onnx", input_name="l_x_"),
         data_directory="tests/data/XpSampledMeanSpectrum",
+        data_column="flux",
     )
 
     catalog = votable_generator.get_data()
